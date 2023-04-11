31bf7ab2a48c45db9ff733ead1c7c584f85e659c
Mela231 <espinozamelani290@gmail.com>
2023-04-11 16:55:39 -0600

    Update
    Se limpio el main ,y se incorporo los metodos en la clase juego

--- ProyectoEstructura/src/proyectoestructura/Juego.java
+++ ProyectoEstructura/src/proyectoestructura/Juego.java
@@ -1,18 +1,130 @@
 package proyectoestructura;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Juego {
     
-    //Invocamos clase Cola
+    private int FILAS = 5;
+    private int COLUMNAS = 10;
+    
+    
+    public void batalla() {
+        //Prueba para crear una matriz de personajes
+        Cola colaPersonajes = agregarPersonajesUsuario();
+        
+        Personaje[][] matriz = new Personaje[FILAS][COLUMNAS];
+        // Pelear hasta que solo quede un personaje en la cola
+       
+        
+        while (!colaPersonajes.estaVacia()) { 
+         
+           Nodo nodoPersonaje1 = colaPersonajes.atiende();
+           Personaje personaje1 = nodoPersonaje1.getDato();
+           Personaje personaje2 = crearPersonajeAleatorio();
+
+            if (personaje1.getFuerza() > personaje2.getFuerza()) {
+                System.out.println(personaje1.getNombre() + " ha ganado!");
+            } else if (personaje2.getFuerza() > personaje1.getFuerza()) {
+                System.out.println(personaje2.getNombre() + " ha ganado!");
+            } else {
+                System.out.println("¡Empate!");
+            }
+
+
+            // Mover personajes hacia el centro
+            for (int i = 0; i < FILAS; i++) {
+                for (int j = 0; j < COLUMNAS / 2; j++) {
+                    if (j == COLUMNAS / 2 - 1) {
+                        matriz[i][j] = personaje1;
+                        matriz[i][COLUMNAS - j - 1] = personaje2;
+                    } else {
+                        matriz[i][j] = matriz[i][j + 1];
+                        matriz[i][COLUMNAS - j - 1] = matriz[i][COLUMNAS - j - 2];
+                    }
+                }
+            }
+
+            // Mostrar la matriz actualizada
+            imprimirPersonajesMovibles(matriz, 5);
+
+            // Esperar 2 segundos
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public Cola agregarPersonajesUsuario() {
+    Scanner scanner = new Scanner(System.in);
     Cola cola = new Cola();
+
+    System.out.println("Ingrese los nombres y la fuerza de los personajes"
+            + " que desea agregar:");
+    for (int i = 0; i < 5; i++) {
+        System.out.print("Nombre del personaje " + (i+1) + ": ");
+        String nombre = scanner.next();
+
+        System.out.print("Fuerza del personaje " + (i+1) + ": ");
+        int fuerza = scanner.nextInt();
+
+        Personaje personaje = new Personaje(nombre, fuerza);
+        Nodo nodo = new Nodo(personaje); // crear nuevo Nodo que contenga el Personaje
+        cola.agregar(nodo); // agregar el nuevo Nodo a la cola
+    }
+
+    return cola;
+    }
+    public Personaje crearPersonajeAleatorio() {
+    Random rand = new Random();
+    int tipo = rand.nextInt(3); // Genera un número aleatorio entre 0 y 2
+    String nombre = "";
+    int fuerza = 0;
     
-    public void agregarPersonaje(Nodo nuevoNodo) {
-        cola.agregar(new Nodo(new Personaje("Arturo",5)));
-        cola.agregar(new Nodo(new Personaje("Carlos",5)));
-        cola.agregar(new Nodo(new Personaje("Ramiro",5)));
+
+    switch (tipo) {
+        case 0: // Arquero
+            nombre = "A";
+            fuerza = 1; // Genera un número aleatorio entre 5 y 9
+            
+            break;
+        case 1: // Caballero
+            nombre = "C";
+            fuerza = 2; // Genera un número aleatorio entre 8 y 13
+            
+            break;
+        case 2: // Mago
+            nombre = "M";
+            fuerza = 3; // Genera un número aleatorio entre 3 y 6
+           
+            break;
+    }
+
+    return new Personaje(nombre, fuerza);
     }
     
-    public void ImprimirPersonaje(){
-        cola.imprimir();
-    }
+    public void imprimirPersonajesMovibles(Personaje[][] matriz, int indice) {
+    System.out.println("----------------------------");
+
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print("|");
+        for (int j = 0; j < matriz[0].length; j++) {
+        if (matriz[i][j] == null) {
+                System.out.print("- ");
+        } else if (i == 0 && (j < indice || j > matriz[0].length-1-indice)) {
+                System.out.print("- ");
+        } else if (matriz[i][j] == null) {
+                System.out.print("- ");
+        }else {
+            System.out.print(matriz[i][j].getNombre() + "(" + 
+                    matriz[i][j].getFuerza() + ")");
+            matriz[i][j] = null; // establecer la posición actual en null
+        }   
+        }
+            System.out.println("|");
+        }
+        System.out.println("----------------------------");
+        }
     
 }
